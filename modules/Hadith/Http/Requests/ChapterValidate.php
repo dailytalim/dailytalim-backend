5fee3973bdb7a83a0ad55107cb053d2a95a93990
Mehedi Jaman <mail4mjaman@gmail.com>
2025-01-21 13:13:24 +0600

    update

--- modules/Hadith/Http/Requests/ChapterValidate.php
+++ modules/Hadith/Http/Requests/ChapterValidate.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Hadith\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Modules\Support\Http\Requests\Request;
 
 class ChapterValidate extends Request
@@ -9,6 +10,10 @@ class ChapterValidate extends Request
     public function rules(): array
     {
         return [
+            'kitab_id' => Rule::validate([
+                'required',
+                
+            ]),
             'name' => 'required',
         ];
     }
