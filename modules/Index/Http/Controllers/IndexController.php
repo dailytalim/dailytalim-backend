daaec198176cfa4a19e4f9b48d8e414c261da237
Mehedi Jaman <mail4mjaman@gmail.com>
2025-03-06 11:23:47 +0600

    update: index route json output

--- modules/Index/Http/Controllers/IndexController.php
+++ modules/Index/Http/Controllers/IndexController.php
@@ -3,14 +3,30 @@
 namespace Modules\Index\Http\Controllers;
 
 use Modules\Hadith\Models\Hadith;
+use Modules\Hadith\Models\Kitab;
 use Modules\Support\Http\Controllers\SiteController;
 
 class IndexController extends SiteController
 {
     public function index()
     {
-        $hadith = Hadith::inRandomOrder()->first();
+        $kitabs = Kitab::select('id', 'name') // Select only needed columns
+            ->with([
+                'chapters' => function ($query) {
+                    $query->select('id', 'kitab_id', 'name'); // Select only necessary columns for chapters
+                },
+                'chapters.hadiths' => function ($query) {
+                    $query->select('id', 'chapter_id', 'description'); // Select only necessary columns for hadiths
+                }
+            ])->get();
 
-        return view('index::index', compact('hadith'));
+        $todaysHadith = Hadith::inRandomOrder()->select('description')->first();
+
+        return [
+            'kitabs' => $kitabs,
+            'todaysHadith' => $todaysHadith,
+        ];
+
+        // return view('index::index', compact('hadith'));
     }
 }
