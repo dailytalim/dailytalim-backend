8061c50426186236241d2884fe4e61e3d4e92afb
Mehedi Jaman <mail4mjaman@gmail.com>
2025-03-07 12:52:59 +0600

    update: API

--- modules/Index/Http/Controllers/IndexController.php
+++ modules/Index/Http/Controllers/IndexController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Modules\Index\Http\Controllers;
 
 use Modules\Hadith\Models\Hadith;
@@ -12,18 +11,18 @@ class IndexController extends SiteController
     {
         $kitabs = Kitab::select('id', 'name') // Select only needed columns
             ->with([
-                'chapters' => function ($query) {
+                'chapters'         => function ($query) {
                     $query->select('id', 'kitab_id', 'name'); // Select only necessary columns for chapters
                 },
                 'chapters.hadiths' => function ($query) {
-                    $query->select('id', 'chapter_id', 'description'); // Select only necessary columns for hadiths
-                }
+                    $query->select('id', 'chapter_id', 'ar', 'bn', 'en'); // Select only necessary columns for hadiths
+                },
             ])->get();
 
-        $todaysHadith = Hadith::inRandomOrder()->select('description')->first();
+        $todaysHadith = Hadith::inRandomOrder()->select('ar', 'bn', 'en')->first();
 
         return [
-            'kitabs' => $kitabs,
+            'kitabs'       => $kitabs,
             'todaysHadith' => $todaysHadith,
         ];
 
