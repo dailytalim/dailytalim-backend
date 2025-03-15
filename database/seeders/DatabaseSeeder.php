e3a9a18bc7efd11eb422e56051e414bd8f7b9903
Mehedi Jaman <mail4mjaman@gmail.com>
2025-03-15 20:02:03 +0600

    update: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
@@ -7,13 +6,9 @@ use Modules\Acl\Database\Seeders\AclModelHasRolesSeeder;
 use Modules\Acl\Database\Seeders\AclPermissionSeeder;
 use Modules\Acl\Database\Seeders\AclRoleSeeder;
 use Modules\Blog\Database\Seeders\BlogAclSeeder;
-use Modules\Blog\Database\Seeders\BlogSeeder;
 use Modules\Hadith\Database\Seeders\ChapterAclSeeder;
-use Modules\Hadith\Database\Seeders\ChapterSeeder;
 use Modules\Hadith\Database\Seeders\HadithAclSeeder;
-use Modules\Hadith\Database\Seeders\HadithSeeder;
 use Modules\Hadith\Database\Seeders\KitabAclSeeder;
-use Modules\Hadith\Database\Seeders\KitabSeeder;
 use Modules\User\Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
@@ -31,10 +26,10 @@ class DatabaseSeeder extends Seeder
             ChapterAclSeeder::class,
             HadithAclSeeder::class,
 
-            BlogSeeder::class,
-            KitabSeeder::class,
-            ChapterSeeder::class,
-            HadithSeeder::class,
+            // BlogSeeder::class,
+            // KitabSeeder::class,
+            // ChapterSeeder::class,
+            // HadithSeeder::class,
 
         ]);
     }
